5c95cf1dad0772235b5048bb95dc16f480d7683e
Ben <manglebot@users.noreply.github.com>
2023-11-12 13:28:09 +0000

    fix:turn .then and .catch into try and catch

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,43 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
+
+const fetchData = async (apiEndPoint) => {
   fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+    try{
+        const response = await fetch(apiEndPoint);
+        const json = await response.json();
+        console.log(json);
+    }
+    catch (error) {
+      console.log(error);
+    }
+    
 };
 
 fetchData(jsonTypicode);
+
+
+
+	// try {
+	// 	const result = await promise;
+	// 	console.log(`Yay! Promise resolved with response: ${result}`)
+	// }
+	// catch (error) {
+	// 	console.log	(`Boo. Promise rejected with response: ${error}`)
+	// }
+
+
+
+// import fetch from "node-fetch";
+
+// const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+// fetchData(jsonTypicode);
